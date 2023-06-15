ca50b3633d7ca57c790ba41bde49049b5e5c3970
userfiona <fionaramaesela@gmail.com>
2023-06-15 12:39:18 -0700

    status

---
+++ 0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a doubly linked list
+ * @h: pointer to head node
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+    size_t num = 0;
+
+    while (h != NULL)
+    {
+        printf("%i\n", h->n);
+        h = h->next;
+        num++;
+    }
+    return (num);
+}
---
+++ 4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees a doubly linked list.
+ * @head: Pointer to the head of the list.
+ *
+ * Return: Void.
+ */
+void free_dlistint(dlistint_t *head)
+{
+    dlistint_t *current;
+
+    while (head != NULL)
+    {
+        current = head;
+        head = head->next;
+        free(current);
+    }
+}
+

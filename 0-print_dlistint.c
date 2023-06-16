a9c585aa1f116ba5b6e855fbaed9a6960ec48773
userfiona <fionaramaesela@gmail.com>
2023-06-15 17:23:44 -0700

    status

--- 0-print_dlistint.c
+++ 0-print_dlistint.c
@@ -2,19 +2,20 @@
 
 /**
  * print_dlistint - prints all the elements of a doubly linked list
- * @h: pointer to head node
+ * @h: pointer to the head node
  *
- * Return: number of nodes
+ * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t num = 0;
-
+    size_t count = 0;
+    
     while (h != NULL)
     {
-        printf("%i\n", h->n);
+        printf("%d\n", h->n);
         h = h->next;
-        num++;
+        count++;
     }
-    return (num);
+    
+    return count;
 }

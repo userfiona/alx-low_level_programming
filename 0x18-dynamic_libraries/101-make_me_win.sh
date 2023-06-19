475a20f03aa16baad85e81c857adc3ff09ffa9e3
userfiona <fionaramaesela@gmail.com>
2023-06-19 08:55:33 -0700

    copy

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,11 +1,1 @@
-#include <stdio.h>
-
-void srand(unsigned int seed)
-{
-    const char nums_txt[] = "9 8 10 24 75 - 9\n";
-    const char txt[] = "Congratulations, you win the Jackpot!\n";
-
-    (void)seed;
-    fputs(nums_txt, stdout);
-    fputs(txt, stdout);
-}
+d52e6c18e0723f5b025a75dea19ef365  gm

4babb1da6c74ad7a8a6582fefcefd3234dbc78d9
userfiona <fionaramaesela@gmail.com>
2023-06-19 07:56:29 -0700

    copy

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-/**
- * srand - Overwrites the srand function in the libc shared object
- * @seed: The seed to use for the random number generator. [Will be Ignored]
- */
 void srand(unsigned int seed)
 {
-    fputs("9 8 10 24 75 - 9\nCongratulations, you win the Jackpot!\n", stdout);
-    exit(EXIT_SUCCESS);
+    const char *nums_txt = "9 8 10 24 75 - 9\n";
+    const char *txt = "Congratulations, you win the Jackpot!\n";
+
+    (void)seed;
+    fwrite(nums_txt, sizeof(char), 17, stdout);
+    fwrite(txt, sizeof(char), 38, stdout);
+    _exit(EXIT_SUCCESS);
 }

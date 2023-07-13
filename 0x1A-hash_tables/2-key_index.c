4a6060ca8c9d779524ed4a7b0af1a8fbc82d195f
userfiona <fionaramaesela@gmail.com>
2023-07-12 19:53:45 -0700

    code

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -8,14 +8,6 @@
  *
  * Return: Always EXIT_SUCCESS.
  */
-
- /**
- * key_index - Calculates the index for a key/value pair in the hash table array.
- * @key: The key used for hashing.
- * @size: The size of the hash table array.
- *
- * Return: The index at which the key/value pair should be stored.
- */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
     unsigned long int hash_value = hash_djb2(key);

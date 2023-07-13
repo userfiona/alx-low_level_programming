e8e2d26ce206823bf0f2ae454e2caa12458fb580
userfiona <fionaramaesela@gmail.com>
2023-07-12 20:03:53 -0700

    code

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,29 +1,42 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - Retrieves the value associated with a key.
- * @ht: Pointer to the hash table structure.
- * @key: Key to match.
+ * hash_table_set - Adds an item to a hash table
+ * @ht: The hash table
+ * @key: Key of the item
+ * @value: Value of the item
  *
- * Return: The value associated with the key, or NULL if the key is not found.
+ * Return: 1 for success, 0 for failure
  */
-char *hash_table_get(const hash_table_t *ht, const char *key)
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *ptr = NULL;
-	unsigned int index;
+	unsigned long int index;
+	hash_node_t *new_node, *tmp_node;
 
-	if (ht && key)
+	if (!ht || !key || !(*key))
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+	tmp_node = ht->array[index];
+	while (tmp_node)
 	{
-		index = key_index((unsigned char *)key, ht->size);
-		ptr = ht->array[index];
-		if (ptr == NULL)
-			return NULL;
-
-		while (strcmp(ptr->key, key) != 0)
-			ptr = ptr->next;
-
-		return ptr->value;
+		if (strcmp(tmp_node->key, key) == 0)
+		{
+			free(tmp_node->value);
+			tmp_node->value = strdup(value);
+			return (1);
+		}
+		tmp_node = tmp_node->next;
 	}
 
-	return NULL;
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
 }

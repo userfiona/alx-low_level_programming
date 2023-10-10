df67e4ec46c32c39ed75678383e15e1298e81f73
userfiona <fionaramaesela@gmail.com>
2023-10-10 11:07:19 -0700

    rythme

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,38 +1,51 @@
 #include "search_algos.h"
-#include <math.h>
 
 /**
- * jump_search - Searches for a value in a sorted array of integers using the
- * Jump search algorithm
- * @array: Pointer to the first element of the array to search in
- * @size: Number of elements in the array
- * @value: Value to search for
+ * interpolation_search - searches for a value in an array of
+ * integers using the Interpolation search algorithm
  *
- * Return: First index where value is located, or -1 if value is not present
- *         in array or if array is NULL
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
-int jump_search(int *array, size_t size, int value)
+int interpolation_search(int *array, size_t size, int value)
 {
-	size_t step = sqrt(size);
-	size_t prev = 0;
+	size_t pos, low, high;
+	double f;
 
-	if (!array || size == 0)
+	if (array == NULL)
 		return (-1);
 
-	while (step < size && array[step] < value)
-	{
-		printf("Value checked array[%lu] = [%d]\n", step, array[step]);
-		prev = step;
-		step += sqrt(size);
-	}
+	low = 0;
+	high = size - 1;
 
-	printf("Value found between indexes [%lu] and [%lu]\n", prev, step);
-
-	for (size_t i = prev; i <= step && i < size; i++)
+	while (size)
 	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
+		f = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
+		pos = (size_t)(low + f);
+		printf("Value checked array[%d]", (int)pos);
+
+		if (pos >= size)
+		{
+			printf(" is out of range\n");
+			break;
+		}
+		else
+		{
+			printf(" = [%d]\n", array[pos]);
+		}
+
+		if (array[pos] == value)
+			return ((int)pos);
+
+		if (array[pos] < value)
+			low = pos + 1;
+		else
+			high = pos - 1;
+
+		if (low == high)
+			break;
 	}
 
 	return (-1);
